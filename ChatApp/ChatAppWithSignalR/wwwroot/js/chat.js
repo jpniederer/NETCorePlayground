39ca71d2a5ea95ac13b028237b46301c4e989488
Joshua Niederer <josh@jpniederer.com>
2018-06-29 22:54:11 -0500

    Starting to play with SignalR to build a chat app. Used the Getting Started post from MSDN.

---
+++ ChatApp/ChatAppWithSignalR/wwwroot/js/chat.js
@@ -0,0 +1,21 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const encodedMsg = user + ": " + msg;
+    const li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(err => console.error(err.toString()));
+
+document.getElementById("sendButton").addEventListener("click", event => {
+    const user = document.getElementById("userInput").value;
+    const messageControl = document.getElementById("messageInput");
+    connection.invoke("SendMessage", user, messageControl.value).catch(err => console.error(err.toString()));
+    event.preventDefault();
+    messageControl.value = "";
+});
