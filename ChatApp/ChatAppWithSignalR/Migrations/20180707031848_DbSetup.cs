b04f2cf3cd588d5eb4a09e13844236f1042ad0df
Joshua Niederer <josh@jpniederer.com>
2018-07-06 22:45:42 -0500

    Setting up the db and basic select/insert operations.

---
+++ ChatApp/ChatAppWithSignalR/Migrations/20180707031848_DbSetup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatAppWithSignalR.Migrations
+{
+    public partial class DbSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatRooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatRooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RoomId = table.Column<Guid>(nullable: false),
+                    Contents = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: false),
+                    PostedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatRooms");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
